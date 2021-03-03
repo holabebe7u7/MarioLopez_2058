3df75ddc9483689824f20853ce3c890bb50e3964
holabebe7u7 <48141168+holabebe7u7@users.noreply.github.com>
2021-03-02 18:36:31 -0600

    Create programa01.js

---
+++ programa01.js
@@ -0,0 +1,16 @@
+  
+<!DOCTYPE html>
+ <html>
+  <body>
+    <button onclick ="function1()"> preciona</button>
+      <p id= "element1">Bienvenido</p>
+    
+    <script>
+        function function1(){
+          document.getElemtByID("element1").innerHTML ="Hola mundo";
+      }
+
+    </script>
+  </body>
+ </html>
+ 
